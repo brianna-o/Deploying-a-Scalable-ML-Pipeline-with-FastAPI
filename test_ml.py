04852c707aba0b09c71132595b3c6721c7f62e21
Brianna <1briannaowens@gmail.com>
2025-11-26 11:41:51 -0800

    Updated test_ml.py and added to screenshots

--- test_ml.py
+++ test_ml.py
@@ -1,30 +1,46 @@
-# import pytest
-# TODO: add necessary import
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, compute_model_metrics, inference
 
 # TODO: implement the first test. Change the function name and input as needed
 
 
-def test_one():
+def test_model():
     """
-    # add description for the first test
+    # Testing that my  model is a RandomForestClassifier
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1], [1, 0], [1, 1]])
+    y = np.array([0, 1, 1])
+
+    model = train_model(X, y)
+
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_inference():
     """
-    # add description for the second test
+    # Verifying that the inference returns the same number of predictions as input rows
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1], [1, 0], [1, 1]])
+    y = np.array([0, 1, 1])
+
+    model = train_model(X, y)
+    preds = inference(model, X)
+
+    assert len(preds) == X.shape[0]
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_model_value():
     """
-    # add description for the third test
+    # Checks that metric function returns numeric value for precision, recall, and fbeta
     """
-    # Your code here
-    pass
+    y_true = np.array([0, 1, 1, 0])
+    y_pred = np.array([0, 1, 0, 0])
+
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
